5f6382f1d44965ef6036f33267f4f5f88c15f401
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2024-01-08 10:08:34 +0200

    Create FishTank.java

---
+++ FishTank.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FishTank {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int length = Integer.parseInt(scanner.nextLine());
+        int width = Integer.parseInt(scanner.nextLine());
+        int height = Integer.parseInt(scanner.nextLine());
+        double percent = Double.parseDouble(scanner.nextLine());
+
+        double fishTankVolumeInLiters = (length * width * height) * 0.001;
+
+        double neededLiters = fishTankVolumeInLiters * (1 - percent/100);
+        System.out.println(neededLiters);
+    }
+}
