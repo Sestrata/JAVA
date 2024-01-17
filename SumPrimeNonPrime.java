487729304d96f3e953ad99f9db57630915e4fbef
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2024-01-17 18:45:08 +0200

    Create SumPrimeNonPrime.java

---
+++ SumPrimeNonPrime.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class SumPrimeNonPrime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        int primeSum = 0;
+        int nonPrimeSum = 0;
+
+        while (!"stop".equals(input)) {
+            int num = Integer.parseInt(input);
+
+            if (num < 0) {
+                System.out.println("Number is negative.");
+                input = scanner.nextLine();
+                continue;
+            }
+
+            boolean isPrime = true;
+
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                primeSum += num;
+            } else {
+                nonPrimeSum += num;
+            }
+
+            input = scanner.nextLine();
+        }
+
+        System.out.printf("Sum of all prime numbers is: %d%n", primeSum);
+        System.out.printf("Sum of all non prime numbers is: %d%n", nonPrimeSum);
+    }
+}
